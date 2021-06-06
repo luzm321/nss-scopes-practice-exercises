45dd38034a1ff9e1e803881467b8435061cb798d
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-06-06 16:34:34 -0500

    completed more scope exercises

--- main.js
+++ main.js
@@ -24,4 +24,19 @@ console.log(conjoinedWords);
 
 //------------------------------Mod Squad Exercise:-------------------------------------------------
 
+const modSquad = {
+    "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+    "series": {
+        "start": "1968",
+        "end": "1973"
+    }
+};
+
+let HTMLRepresentation = `<h1>The Mod Squad</h1>`;
+
+modSquad.members.forEach(member => {
+    HTMLRepresentation += `<div>${member}</div>`;
+});
+
+console.log(HTMLRepresentation);
 
